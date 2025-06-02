659e00786b25085645135270600d037837be33c7
teklu abayneh <tekluabayneh@gmail.com>
2025-06-02 19:22:26 +0300

    test: mocked axios and improved FollowersList.test.js

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -1,48 +1,73 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from "@testing-library/react";
 import FollowersList from "../FollowersList";
+import { MemoryRouter } from "react-router-dom";
+import axios from "axios";
+jest.mock("axios");
 
-const MockFollowersList = () => {
-    return (
-        <BrowserRouter>
-            <FollowersList />
-        </BrowserRouter>
-    )
-}
+describe("follower list", () => {
+  it("should render follower items", async () => {
+    const mockedResponse = {
+      data: {
+        results: [
+          {
+            login: { uuid: "1", username: "john_doe" },
+            name: { first: "John", last: "Doe" },
+            picture: { large: "some-url" },
+          },
+        ],
+      },
+    };
 
-describe("FollowersList", () => {
+    axios.get.mockResolvedValueOnce(mockedResponse);
 
-    beforeEach(() => {
-        // console.log("RUNS BEFORE EACH TEST")
-        jest.mock("../../../__mocks__/axios")
-    })
+    render(
+      <MemoryRouter>
+        <FollowersList />
+      </MemoryRouter>
+    );
 
-    // beforeAll(() => {
-    //     console.log("RUNS ONCE BEFORE ALL TESTS")
-    // })
+    const followerDivElement = await screen.findByTestId("follower-item-0");
+    expect(followerDivElement).toBeInTheDocument();
+  });
 
-    // afterEach(() => {
-    //     console.log("RUNS AFTER EACH TEST")
-    // })
+  it("should  render multiple followers items", async () => {
+    const mockedResponse = {
+      data: {
+        results: [
+          {
+            login: { uuid: "1", username: "john_doe" },
+            name: { first: "John", last: "Doe" },
+            picture: { large: "some-url" },
+          },
+          {
+            login: { uuid: "1", username: "john_doe" },
+            name: { first: "John", last: "Doe" },
+            picture: { large: "some-url" },
+          },
+          {
+            login: { uuid: "1", username: "john_doe" },
+            name: { first: "John", last: "Doe" },
+            picture: { large: "some-url" },
+          },
+        ],
+      },
+    };
 
-    // afterAll(() => {
-    //     console.log("RUNS ONCE AFTER ALL TESTS")
-    // })
+    axios.get.mockResolvedValueOnce(mockedResponse);
 
-    it('should fetch and render input element', async () => {
-        render(
-            <MockFollowersList />
-        );
-        const followerDivElement = await screen.findByTestId(`follower-item-0`)
-        expect(followerDivElement).toBeInTheDocument();
-    });
-    
-    it('should fetch and render input element', async () => {
-        render(
-            <MockFollowersList />
-        );
-    
-        const followerDivElement = await screen.findByTestId(`follower-item-0`)
-        expect(followerDivElement).toBeInTheDocument();
-    });
-})
+    render(
+      <MemoryRouter>
+        <FollowersList />
+      </MemoryRouter>
+    );
+
+    for (let i = 0; i < 3; i++) {
+      const item = await screen.findByTestId(`follower-item-${i}`);
+      expect(item).toBeInTheDocument();
+    }
+
+    const followerDivElement = await screen.findAllByTestId(/follower-item/i);
+
+    expect(followerDivElement.length).toBe(3);
+  });
+});
